dd9ebc59745a8489ff309c15d5e98a14dc8e1843
ravine13 <kuriaderek@gmail.com>
2023-11-04 05:33:53 +0300

    creating components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from "react";
-import ColorList from "./ColorList";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
+import ProjectList from "./ProjectList";
+import user from "../data/user";
 
 function App() {
   return (
     <div>
-      <ColorList />
+      <NavBar />
+      <Home username={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} links={user.links} />
+      <ProjectList projects={user.projects} />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NavBar() {
+  const links = ["home", "about", "projects"];
+
+  return (
+    <nav>
+      {links.map((link, index) => (
+        <a key={index} href={`#${link}`}>
+          {link}
+        </a>
+      ))}
+    </nav>
+  );
+}
+
+export default NavBar;
